450f635b3a7477bb03ae2711b153fe3339dbe97a
Aurimas Liutikas <aurimas@google.com>
2017-11-14 18:27:33 -0800

    Migrate packages/apps/Car to aapt2
    Test: None
    Change-Id: If81e8e9ca0ff5a6a5a822f35237608c0fd4109dc
    Merged-In: If81e8e9ca0ff5a6a5a822f35237608c0fd4109dc
    (cherry picked from commit e903c4268f1f7ec7a99737cb98e4006c65a95613)

--- Android.mk
+++ Android.mk
@@ -22,8 +22,9 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
+LOCAL_USE_AAPT2 := true
+
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-LOCAL_RESOURCE_DIR += frameworks/support/design/res
 
 LOCAL_PACKAGE_NAME := CarLensPickerApp
 LOCAL_PRIVATE_PLATFORM_APIS := true
@@ -32,17 +33,15 @@ LOCAL_MODULE_TAGS := optional
 
 include packages/apps/Car/libs/car-stream-ui-lib/car-stream-ui-lib.mk
 
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-design
+LOCAL_STATIC_ANDROID_LIBRARIES += \
+    android-support-v4 \
+    android-support-design
+
 LOCAL_STATIC_JAVA_LIBRARIES += car-stream-lib
 
-LOCAL_AAPT_FLAGS += --extra-packages android.support.design
-
 # Include support-v7-appcompat, if not already included
-ifeq (,$(findstring android-support-v7-appcompat,$(LOCAL_STATIC_JAVA_LIBRARIES)))
-LOCAL_RESOURCE_DIR += frameworks/support/v7/appcompat/res
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
+ifeq (,$(findstring android-support-v7-appcompat,$(LOCAL_STATIC_ANDROID_LIBRARIES)))
+LOCAL_STATIC_ANDROID_LIBRARIES += android-support-v7-appcompat
 endif
 
 LOCAL_PROGUARD_ENABLED := disabled
